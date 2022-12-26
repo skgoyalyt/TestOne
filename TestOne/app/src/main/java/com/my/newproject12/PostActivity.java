49319780378759e4a289f9e075ed8bcf6351b4df
skgoyalyt <96329160+skgoyalyt@users.noreply.github.com>
2022-12-26 14:06:32 +0530

    Add Section testOne
    Description testOne

---
+++ TestOne/app/src/main/java/com/my/newproject12/PostActivity.java
@@ -0,0 +1,201 @@
+package com.my.newproject12;
+
+import android.animation.*;
+import android.app.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.Typeface;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.os.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.view.*;
+import android.view.View.*;
+import android.view.animation.*;
+import android.webkit.*;
+import android.widget.*;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import androidx.annotation.*;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.*;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+import androidx.recyclerview.widget.RecyclerView.ViewHolder;
+import java.io.*;
+import java.text.*;
+import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.*;
+import org.json.*;
+
+public class PostActivity extends AppCompatActivity {
+	
+	private ArrayList<HashMap<String, Object>> lm = new ArrayList<>();
+	
+	private LinearLayout linear1;
+	private LinearLayout linear2;
+	private CardView cardview1;
+	private TextView textview1;
+	private TextView textview3;
+	private TextView textview4;
+	private RecyclerView recyclerview1;
+	private ImageView imageview1;
+	private LinearLayout linear3;
+	private ImageView imageview2;
+	private ImageView imageview3;
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.post);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+		linear1 = findViewById(R.id.linear1);
+		linear2 = findViewById(R.id.linear2);
+		cardview1 = findViewById(R.id.cardview1);
+		textview1 = findViewById(R.id.textview1);
+		textview3 = findViewById(R.id.textview3);
+		textview4 = findViewById(R.id.textview4);
+		recyclerview1 = findViewById(R.id.recyclerview1);
+		imageview1 = findViewById(R.id.imageview1);
+		linear3 = findViewById(R.id.linear3);
+		imageview2 = findViewById(R.id.imageview2);
+		imageview3 = findViewById(R.id.imageview3);
+	}
+	
+	private void initializeLogic() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+			int flags = getWindow().getDecorView().getSystemUiVisibility();
+			
+			flags |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+			
+			getWindow().getDecorView().setSystemUiVisibility(flags);
+			
+			getWindow().setStatusBarColor(Color.WHITE);
+		};
+		textview1.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/product_sans_regular.ttf"), 1);
+		textview3.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/product_sans_.ttf"), 0);
+		textview4.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/product_sans_regular.ttf"), 1);
+		for(int _repeat16 = 0; _repeat16 < (int)(4); _repeat16++) {
+			{
+				HashMap<String, Object> _item = new HashMap<>();
+				_item.put("", "");
+				lm.add(_item);
+			}
+			
+		}
+		recyclerview1.setAdapter(new Recyclerview1Adapter(lm));
+		recyclerview1.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL, false));
+	}
+	
+	public class Recyclerview1Adapter extends RecyclerView.Adapter<Recyclerview1Adapter.ViewHolder> {
+		
+		ArrayList<HashMap<String, Object>> _data;
+		
+		public Recyclerview1Adapter(ArrayList<HashMap<String, Object>> _arr) {
+			_data = _arr;
+		}
+		
+		@Override
+		public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+			LayoutInflater _inflater = getLayoutInflater();
+			View _v = _inflater.inflate(R.layout.posts, null);
+			RecyclerView.LayoutParams _lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+			_v.setLayoutParams(_lp);
+			return new ViewHolder(_v);
+		}
+		
+		@Override
+		public void onBindViewHolder(ViewHolder _holder, final int _position) {
+			View _view = _holder.itemView;
+			
+			final LinearLayout mbg = _view.findViewById(R.id.mbg);
+			final androidx.cardview.widget.CardView cardview1 = _view.findViewById(R.id.cardview1);
+			final TextView txt_category = _view.findViewById(R.id.txt_category);
+			final TextView txt_title = _view.findViewById(R.id.txt_title);
+			final ImageView imageview1 = _view.findViewById(R.id.imageview1);
+			
+			RecyclerView.LayoutParams _lp = new RecyclerView.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+			_view.setLayoutParams(_lp);
+			mbg.setBackground(new GradientDrawable() { public GradientDrawable getIns(int a, int b, int c, int d) { this.setCornerRadius(a); this.setStroke(b, c); this.setColor(d); return this; } }.getIns((int)20, (int)1, 0xFF9E9E9E, 0xFFFFFFFF));
+			txt_category.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/product_sans_.ttf"), 0);
+			txt_title.setTypeface(Typeface.createFromAsset(getAssets(),"fonts/product_sans_regular.ttf"), 1);
+		}
+		
+		@Override
+		public int getItemCount() {
+			return _data.size();
+		}
+		
+		public class ViewHolder extends RecyclerView.ViewHolder {
+			public ViewHolder(View v) {
+				super(v);
+			}
+		}
+	}
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
